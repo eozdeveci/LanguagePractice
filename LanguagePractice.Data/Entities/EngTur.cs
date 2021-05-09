fc16c3ae940749633f89f2cf960e3f29ddbe5824
Emre ÖZDEVECİ <eozdeveci@gmail.com>
2021-05-09 23:42:06 +0300

    Added Postgresql connection string and created first table.

---
+++ LanguagePractice.Data/Entities/EngTur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePractice.Data.Entities
+{
+    public class EngTur : BaseEntity
+    {
+        public string EnglishWord { get; set; }
+        public string TurkishWord { get; set; }
+        public byte TotalRepeatCount { get; set; }
+        public byte RepeatedCount { get; set; }
+    }
+}
---
+++ LanguagePractice.Data/Migrations/20210509200523_AddedFirstMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LanguagePractice.Data.Migrations
+{
+    public partial class AddedFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
